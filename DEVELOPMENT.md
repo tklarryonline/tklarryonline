4209607beece315e6d16211c6014f7493ca0a191
Luan Nguyen <luan.nguyen@redbubble.com>
2024-01-04 13:54:31 +1100

    wip: Add my details

---
+++ DEVELOPMENT.md
@@ -0,0 +1,29 @@
+# How to work on this repo
+
+## Install and Run
+
+Requirements: rspress and bun.
+
+```sh
+cd /path/to/tklarry-site
+
+bun install
+```
+
+## Start dev server
+
+```sh
+bun run dev
+```
+
+## Preview in local environment
+
+```sh
+bun run preview
+```
+
+## Build in production
+
+```sh
+bun run build
+```
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,18 +1,24 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Luan Nguyen',
-  description: 'Full Stuck Engineer',
+  root: path.join(__dirname, "docs"),
+  title: "Luan Nguyen",
+  description: "Full Stuck Engineer",
   icon: "/rspress-icon.png",
   logo: {
     light: "/rspress-light-logo.png",
     dark: "/rspress-dark-logo.png",
   },
   themeConfig: {
+    darkMode: false,
+    footer: {
+      message: "Hellowolrd"
+    },
+    search: false,
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      { icon: "github", mode: "link", content: "https://github.com/tklarryonline" },
+      { icon: "linkedin", mode: "link", content: "https://linkedin.com/in/tklarryonline" },
     ],
   },
 });
